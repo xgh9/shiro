f32ab77f8c502db1685096cac3d18ee8c18002cc
1373983419@qq.com <1373983419@qq.com>
2020-10-03 22:22:01 +0800

    10-3

---
+++ network/src/main/java/com/example/network/config/ShiroConfig.java
@@ -0,0 +1,12 @@
+package com.example.network.config;
+
+import org.apache.shiro.realm.Realm;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ShiroConfig {
+
+//    public Realm getReam(){
+//
+//    }
+}
